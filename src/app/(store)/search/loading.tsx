d963f8146e1deb5ee6bfde63270ff73afe5db663
Takeo <morilucasaluno@gmail.com>
2024-10-19 05:04:33 -0300

    fix: Add suspense boundaries when using useSearchParams

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,18 +1,18 @@
-'use client'
+
 
 import { Skeleton } from '@/components/ui/skeleton'
-import { useSearchParams } from 'next/navigation'
+import CurrentSearch from './current-search'
+import { Suspense } from 'react'
 
 export default function LoadingSearch() {
-  const searchParams = useSearchParams()
 
-  const query = searchParams.get('q')
 
   return (
     <div className="flex flex-col gap-4 ">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query}</span>
-      </p>
+      
+      <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
 
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="h-[388px] bg-zinc-50/10" />
